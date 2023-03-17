fef7ac309cf7a7b1a6c01e5974b7ebf00cced23d
Samuel Patracao | Stone <samuel.patracao@stone.com.br>
2023-03-17 11:15:26 -0300

    :sparkles: add persisntence and on-off buton

--- addMessage.js
+++ addMessage.js
@@ -1,8 +1,31 @@
+const urlElement = document.getElementById("url");
+const urlToReplaceElement = document.getElementById("replacer");
+const checkbox = document.getElementById("on-off");
+
+function Read() {
+  chrome.runtime.sendMessage({ command: "Read" }, ({onOff, redirectList}) => {
+    console.log(redirectList)
+    checkbox.checked = onOff;
+
+    urlElement.value = redirectList[0].url;
+    urlToReplaceElement.value = redirectList[0].urlToReplace;
+  });
+}
+
 document.querySelector("button").addEventListener("click", () => {
-    const replacer = 
-    {
-      "url": document.getElementById("url").value,
-      "urlToReplace": document.getElementById("replacer").value
-    };
-    chrome.runtime.sendMessage(replacer);
+  const replacer =
+  {
+    "url": urlElement.value,
+    "urlToReplace": urlToReplaceElement.value
+  };
+
+  chrome.runtime.sendMessage({ command: "Replacer", value: replacer });
 });
+
+checkbox.addEventListener("change", () => {
+  const value = checkbox.checked;
+
+  chrome.runtime.sendMessage({ command: "Switch", value });
+});
+
+Read();
--- redirect.js
+++ redirect.js
@@ -1,26 +1,54 @@
+let onOff = false;
+
 let redirectList = [
+    {url: "", urlToReplace: ""}
 ]
 
-chrome.runtime.onMessage.addListener((message) => {
-    redirectList = [
-        message
-    ]
-});
-
-chrome.webRequest.onBeforeRequest.addListener(
-    function (details) {
-        const actualUrl = details.url;
-
-        for(const {url, urlToReplace} of redirectList)
-        {
-            if(!actualUrl.includes(url))
-            {
-                continue;
-            }
-
-            return { redirectUrl: actualUrl.replace(url, urlToReplace) };
-        };
+const commands = {
+    Replacer: (value) => {
+        redirectList = [
+            value
+        ];
     },
-    { urls: ["<all_urls>"] },
-    ["blocking"]
-);
+    Switch: (value) => {
+        onOff = value;
+        
+        SwitchRedirect();
+    },
+    Read: (_, sendResponse) => {
+        sendResponse(
+            {onOff, redirectList}
+        )
+    }
+}
+
+const redirectListener = (details) => {
+    const actualUrl = details.url;
+
+    for(const {url, urlToReplace} of redirectList)
+    {
+        if(!actualUrl.includes(url))
+        {
+            continue;
+        }
+
+        return { redirectUrl: actualUrl.replace(url, urlToReplace) };
+    };
+}
+
+function SwitchRedirect() {
+    if(onOff) {
+        chrome.webRequest.onBeforeRequest.addListener(
+            redirectListener,
+            { urls: ["<all_urls>"] },
+            ["blocking"]
+        );
+    }
+    else {
+        chrome.webRequest.onBeforeRequest.removeListener(redirectListener);
+    }
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    commands[message.command](message.value, sendResponse);
+});
